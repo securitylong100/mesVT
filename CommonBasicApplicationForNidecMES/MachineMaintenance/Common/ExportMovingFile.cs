412c729e4a37566651ad4d9da2c3954111aed293
securitylong100 <30890603+securitylong100@users.noreply.github.com>
2018-06-27 20:52:11 +0700

    update

--- CommonBasicApplicationForNidecMES/MachineMaintenance/Common/ExportMovingFile.cs
+++ CommonBasicApplicationForNidecMES/MachineMaintenance/Common/ExportMovingFile.cs
@@ -16,7 +16,8 @@ namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Common
     {
         public void exportmoving(ref DataGridViewCommon dgv, string codestatus)
         {
-            string datetime = DateTime.Now.ToString("yyyy_mm_dd");
+            string datetime = DateTime.Now.ToString("yyyy-MM-dd");
+            string partname = "";
             try
             {
                 Excel.Application xlApp;
@@ -31,21 +32,25 @@ namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Common
                 {
                     xlWorkSheet.Cells[2, 3] = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "BG/CĐ";
                     xlWorkSheet.Cells[6, 6] = "X";
+                    partname = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "_BG_";
                 }
                 if (codestatus == "Mượn")
                 {
                     xlWorkSheet.Cells[2, 3] = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "M/CĐ";
                     xlWorkSheet.Cells[6, 14] = "X";
+                    partname = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "_M_";
                 }
                 if (codestatus == "Trả")
                 {
                     xlWorkSheet.Cells[2, 3] = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "T/CĐ";
                     xlWorkSheet.Cells[6, 22] = "X";
+                    partname = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "_T_";
                 }
                 if (codestatus == "Thuê")
                 {
                     xlWorkSheet.Cells[2, 3] = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "TH/CĐ";
                     xlWorkSheet.Cells[6, 30] = "X";
+                    partname = dgv.Rows[0].Cells["col_code_name"].Value.ToString() + "_TH_";
                 }
                 #endregion
                 #region factory and reason
@@ -61,7 +66,14 @@ namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Common
                     xlWorkSheet.Cells[i + 15, 3] = dgv.Rows[i].Cells["col_machine_name"].Value.ToString();
                     xlWorkSheet.Cells[i + 15, 17] = dgv.Rows[i].Cells["col_machine_model"].Value.ToString();
                     xlWorkSheet.Cells[i + 15, 23] = dgv.Rows[i].Cells["col_machine_serial"].Value.ToString();
-                    xlWorkSheet.Cells[i + 15, 28] = dgv.Rows[i].Cells["col_machine_costvalue"].Value.ToString();
+                    if (dgv.Rows[i].Cells["col_machine_costvalue"].Value.ToString() == "0")
+                    {
+                        xlWorkSheet.Cells[i + 15, 28] = "";
+                    }
+                    else
+                    {
+                        xlWorkSheet.Cells[i + 15, 28] = dgv.Rows[i].Cells["col_machine_costvalue"].Value.ToString();
+                    }
                     xlWorkSheet.Cells[i + 15, 32] = dgv.Rows[i].Cells["col_comments_machine"].Value.ToString();
                 }
                 #endregion
@@ -72,11 +84,11 @@ namespace Com.Nidec.Mes.Common.Basic.MachineMaintenance.Common
                 xlWorkSheet.Cells[49, 33] = dgv.Rows[0].Cells["col_registration_date_time"].Value.ToString().Substring(0, 4); //year
                 #endregion
 
-                xlWorkBook.SaveAs("D:\\VT CP\\ExportMoving_" + datetime + ".xlsx", Excel.XlFileFormat.xlWorkbookDefault, misValue, misValue, misValue,
+                xlWorkBook.SaveAs("D:\\VT CP\\Moving_" + codestatus+"_" +partname+ datetime + ".xlsx", Excel.XlFileFormat.xlWorkbookDefault, misValue, misValue, misValue,
                  misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
                 MessageBox.Show("Excel file created, you can find in the folder D:\\VT CP", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 xlWorkBook.Close(true, misValue, misValue);
-                xlApp.Workbooks.Open("D:\\VT CP\\ExportMoving_" + datetime + ".xlsx");
+                xlApp.Workbooks.Open("D:\\VT CP\\Moving_" + codestatus + "_" + partname + datetime + ".xlsx");
                 xlApp.Visible = true;
             }
             catch
